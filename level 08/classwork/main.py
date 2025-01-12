67c3ca0b94ee964495ad424bec599eb87ea10440
sabanefaridze13 <neparidzesaba792@gmail.com>
2025-01-12 08:23:39 -0800

    commit

--- level 08/classwork/main.py
+++ level 08/classwork/main.py
@@ -4,8 +4,8 @@ age_int = int(age)
 age_float = float(age) 
 age_str = str(age)  
 
-# int(): აქვევს სხვა ტიპის data-ს integer-ად
-#str(): აქვევს სხვა ტიპის data-ს string-ად
-#float() აქვევს სხვა ტიპის data-ს float-ად
+# int(): აქცევს სხვა ტიპის data-ს integer-ად
+#str(): აქცევს სხვა ტიპის data-ს string-ად
+#float() აქცევს სხვა ტიპის data-ს float-ად
 
 
---
+++ level 14/classwork/main.py
@@ -0,0 +1,26 @@
+# < ნაკლებია
+# > მეტია
+# == ტოლია
+#!= არ არის ტოლი
+#>= მეტია ან ტოლია
+#<= ნაკლებია ან ტოლია
+
+
+
+# 2
+
+#true and false =false
+#true and true=true
+#false and false=false
+#true or false=true
+#true or true =true
+#false or false=false
+
+
+# 3 
+
+# my favourite numb is 13
+# my friend 7
+#( 13 >7)
+#true რადგან მოქმედება სწორია
+
