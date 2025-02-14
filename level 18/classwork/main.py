b18b75af867a95802a0968dfc012c9e8073b627a
sabanefaridze13 <neparidzesaba792@gmail.com>
2025-02-14 05:25:19 -0800

    commit

---
+++ level 18/classwork/main.py
@@ -0,0 +1,35 @@
+# 1
+for i in range(11):
+    print(i)
+
+
+
+# 2
+
+for i in range(50,150):
+    print(i)
+
+# 3
+for i in range(200,500,2):
+    print(i)
+
+# 4
+
+for i in range(1,50,2):
+    print(i)
+
+# 5
+
+start =input("enter start index: ")
+end =input("enter end index: ")
+
+for i in range(start, end):
+    print(i)
+
+
+# 6
+
+# name = input("enter your name: ")
+
+# for i in name:
+#    print(i)
---
+++ level 18/homework/main.py
@@ -0,0 +1,44 @@
+# 1
+
+# for i in name:
+#   print(saba nefaridze)
+
+# 2
+
+for i in range(21):
+    print(i)
+
+# 3
+
+for i in range(20,0,-1):
+    print(i)
+
+# 4
+
+sum = 0
+
+for i in range(1,51):
+    print(i) 
+    sum =sum + i 
+
+    print(sum)
+
+# 5
+
+sum = 0
+
+for i in range(0,11,2):
+    sum += i
+
+print(sum)
+
+# 6
+
+numbers=int(input("enter number: "))
+
+for i in range(0,numbers):
+    if i%2==0:
+        print(str(i)+"is even")
+    else:
+        print(str(i)+"is odd")
+         
